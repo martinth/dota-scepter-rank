9c7c56bfc8df7bc69d994f9eb4dfb162e47d6bb6
Martin Thurau <martin.thurau@gmail.com>
2017-05-21 13:40:27 +0200

    Update hero images with larger version

--- src/app/components/HeroScepter/index.tsx
+++ src/app/components/HeroScepter/index.tsx
@@ -18,10 +18,10 @@ class HeroScepter extends React.Component<IProps, void> {
         <div className={style.card__header}>
           {scepter.heroName}
         </div>
+        <div className={style.card__image}>
+          <img src={require(`./hero-images/${scepter.id}.png`)} alt={scepter.heroName} />
+        </div>
         <div className={style.card__content}>
-          <div className={style.card__image}>
-            <img src={require(`./hero-images/${scepter.id}.png`)} alt={scepter.heroName} />
-          </div>
           <div className={style.card__text}>
             <ul>
               {scepter.details.map((detail, idx) => <li key={idx}>{detail}</li>)}
