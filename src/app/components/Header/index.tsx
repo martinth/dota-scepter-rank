111d99d1d4251673947c597daf0067ba9b424127
Martin Thurau <martin.thurau@gmail.com>
2017-05-21 14:02:33 +0200

    Style navigation

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
 import { Link } from 'react-router';
+const style = require('./style.css');
 
 export const Header = () => (
-  <nav>
-    <ul className="nav">
-      <li className="nav-item active">
-        <Link className="nav-link" to="/">Home</Link>
+  <nav className={style.nav}>
+    <ul className={style.nav__list}>
+      <li className={style.nav__list__icon}>
+        <img src={require('./scepter.png')} />
       </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="compare">Compare</Link>
+      <li className={style.nav__list__item}>
+        <Link to="/">Home</Link>
       </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="scepter-list">Scepter List</Link>
+      <li className={style.nav__list__item}>
+        <Link to="compare">Compare</Link>
       </li>
-  
+      <li className={style.nav__list__item}>
+        <Link to="scepter-list">Scepter List</Link>
+      </li>
     </ul>
   </nav>
 );
